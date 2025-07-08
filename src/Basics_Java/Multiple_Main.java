cedde8d4d122742cbfcac54f903173276880ad9d
Hrishikesh Bhogade <hrishibhogade.20@gmail.com>
2025-07-08 11:10:38 +0530

    PrintSameLine class project

---
+++ src/Basics_Java/Multiple_Main.java
@@ -0,0 +1,16 @@
+package Basics_Java;
+
+public class Multiple_Main {
+        public static void main(String[] args) {
+
+        }
+
+        public static void main(int args) {
+
+        }
+
+        public static void main(String args) {
+
+        }
+    }
+
---
+++ src/Basics_Java_2/Variable_7_interview_QnA.java
@@ -0,0 +1,11 @@
+package Basics_Java_2;
+
+public class Variable_7_interview_QnA {
+    public static void main(String[] args) {
+        int _2 = 123;
+        System.out.println(_2);
+        int _1 = 123;
+        int _0 = 123;
+    }
+}
+
